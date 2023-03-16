52027ab3fc8fd7fe4cfb78ac4302d4e59d1ef1cc
Sanghee <dev.sanghee@gmail.com>
2023-03-17 08:03:42 +0900

    Initial commit

---
+++ BonoBonoDiary/BonoBonoDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonoBonoDiaryApp.swift
+//  BonoBonoDiary
+//
+//  Created by Selina on 2023/03/17.
+//
+
+import SwiftUI
+
+@main
+struct BonoBonoDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DiaryListView()
+        }
+    }
+}
---
+++ BonoBonoDiary/View/DiaryListView.swift
@@ -0,0 +1,26 @@
+//
+//  DiaryListView.swift
+//  BonoBonoDiary
+//
+//  Created by Selina on 2023/03/17.
+//
+
+import SwiftUI
+
+struct DiaryListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct DiaryListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryListView()
+    }
+}
